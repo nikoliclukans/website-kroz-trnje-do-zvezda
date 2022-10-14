878384e5f690fb8ae6827282217396b1b5c1893d
Marko Amidzic <m.amidzic@vegait.rs>
2022-10-14 22:27:20 +0200

    set max item per page for blogs

--- PerAspera/Controllers/Render/BlogsController.cs
+++ PerAspera/Controllers/Render/BlogsController.cs
@@ -21,7 +21,7 @@ namespace PerAspera.Controllers.Render
 			var category = query.GetUintParameter("category");
 			var year = query.GetUintParameter("year");
 			var currentPage = query.GetUintParameter("page") ?? 1;
-			var maxItemPerPage = 6;
+			var maxItemPerPage = 9;
             var page = this.CurrentPage as Blogs;
 			var blogsQuery = this.CurrentPage.Children<BlogPage>();
 			if(category != null)
