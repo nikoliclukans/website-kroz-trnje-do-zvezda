42723ff06d57b93e3827010c7dd9d0b38d3e0108
Andrej Iviciak <andrej.iviciak@vegait.rs>
2022-10-14 21:58:47 +0200

    Contact Us Forma

---
+++ PerAspera/Infrastructure/Implementation/ContactUsEmailTemplate.cs
@@ -0,0 +1,28 @@
+﻿using PerAspera.Infrastructure.Builders.Email;
+
+namespace PerAspera.Infrastructure.Implementation
+{
+	public class ContactUsEmailTemplate : IEmailTemplate, IEmailTemplateBuilder, IEmailTemplateWithData
+    {
+		private string _message;
+		public ContactUsEmailTemplate(string message)
+		{
+			_message = message;
+		}
+
+		public string Build()
+		{
+			return _message;
+		}
+
+		public IEmailTemplateBuilder LoadData()
+		{
+			return this;
+		}
+
+		public IEmailTemplateWithData LoadTemplate()
+		{
+			return this;
+		}
+	}
+}
