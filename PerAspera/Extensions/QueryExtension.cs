d31cb8e26157185bf2228dd2c19ff5cb19e72adb
Nemanja Sobo <n.sobo@vegaitsourcing.rs>
2022-10-14 21:11:00 +0200

    Implement blogs.

---
+++ PerAspera/Extensions/QueryExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace PerAspera.Extensions
+{
+	public static class QueryExtension
+	{
+
+		public static uint? GetUintParameter(this IQueryCollection queryCollection, string key)
+		{
+			if(queryCollection.TryGetValue(key, out var value))
+			{
+				return uint.TryParse(value, out var intPar) ? intPar : null;
+			}
+			return null;
+		}
+	}
+}
---
+++ PerAspera/Models/Extended/Blogs.cs
@@ -0,0 +1,19 @@
+﻿using PerAspera.Models.ViewModels;
+
+namespace PerAspera.Models.Generated
+{
+	public partial class Blogs
+	{
+
+		public PaginatedCollectionViewModel<BlogPage> BlogList { get; set; }
+
+		public string GetPaginatedUrl(int pageNumber)=>$"{this.Url()}?page={pageNumber}&category={this.CurrentCategory}&year={this.CurrentYear}";
+
+		public uint? CurrentCategory { get; set; }
+		public uint? CurrentYear { get; set; }
+
+		public bool IsSelectedCategory(int categoryId) => CurrentCategory == categoryId;
+		public bool IsSelectedYear(int year) => CurrentYear == year;
+		
+	}
+}
