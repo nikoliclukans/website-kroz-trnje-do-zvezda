d9a37df894d20dcd636dd6f6f3d62195a938f3a1
Jovo Šunjka <j.sunjka@vegait.rs>
2022-10-15 15:36:07 +0200

    Added Shop order form

---
+++ PerAspera/Controllers/Surface/ShopOrderFormController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PerAspera.Infrastructure.Implementation;
+using PerAspera.Infrastructure.Interfaces;
+using PerAspera.Models.Generated;
+using PerAspera.Models.ViewModels;
+using System.Text;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace PerAspera.Controllers.Surface
+{
+	public class ShopOrderFormController : SurfaceController
+	{
+		private readonly IEmailService _emailService;
+
+		public ShopOrderFormController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, 
+			ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, 
+			IPublishedUrlProvider publishedUrlProvider, IEmailService emailService) 
+			: base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+		{
+			_emailService = emailService;
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Order(ShopOrderDto mode)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest();
+			}
+
+			return Ok();
+		}
+	}
+}
---
+++ PerAspera/Models/Extended/Shop.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace PerAspera.Models.Generated
+{
+	public partial class Shop
+	{
+		public List<SelectListItem> GetProducts()
+		{
+			List<SelectListItem> products = new List<SelectListItem>
+			{
+				new SelectListItem {Text = "Odaberite predmet za poručivanje...", Value = "1"},
+				new SelectListItem {Text = "Imunomania", Value = "1"},
+				new SelectListItem {Text = "Fearless kapa", Value = "2"},
+			};
+			return products;
+		}
+	}
+}
