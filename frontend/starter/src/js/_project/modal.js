03b8bc7f39a5cd1326dfaaa184bfffb6f8bf9e8e
jovanasobo92 <85742841+jovanasobo92@users.noreply.github.com>
2022-10-15 11:15:07 +0200

    frontend- shop improvements

--- frontend/starter/src/js/_project/modal.js
+++ frontend/starter/src/js/_project/modal.js
@@ -13,8 +13,16 @@ const modalHandler = {
 			$this.addClass('active');
 			slide.eq(thisIndex).addClass('modal-overlay--opened');
 		});
+		$('.shop-card').on('click', '.js-shop-modal-open', function() {
+			$('.js-shop-modal-overlay').removeClass('open');
+			if ($(this).next().is(':hidden')) {
+				$(this).next().addClass('open');
+			} else {
+				$(this).removeClass('open');
+			}
+		});
 		$('.js-modal-close').on('click', () => {
-
+			$('.js-shop-modal-overlay').removeClass('open');
 			$('.js-modal-overlay').removeClass('modal-overlay--opened');
 		});
 	}
