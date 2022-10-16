7fb08d0e607aa0d0faf3999d0ae5db24d44c39fb
jovanasobo92 <85742841+jovanasobo92@users.noreply.github.com>
2022-10-16 15:39:09 +0200

    FE -responsive mobile and tablet

--- frontend/starter/src/js/_project/search.js
+++ frontend/starter/src/js/_project/search.js
@@ -2,14 +2,45 @@
 const search = {
 	searchToggleBtn: document.querySelector('.js-search-open'),
 	search: document.querySelector('.header-search'),
+	$body: $('body'),
+	$win: $(window),
+	topScroll: 0,
+	disabledScrollClass: 'disabled',
+	disableScroll: function() {
+		if (!this.isScrollDisabled) {
+			this.topScroll = this.$win.scrollTop();
+			this.$body.css('top', -this.topScroll + 'px').addClass(this.disabledScrollClass);
+			this.isScrollDisabled = true;
+		}
+	},
+	enableScroll: function() {
+		this.$body.removeAttr('style').removeClass(this.disabledScrollClass);
+		this.$win.scrollTop(this.topScroll);
+		this.isScrollDisabled = false;
+	},
 	init: function() {
 		this.openSearch();
+		this.openNav();
 	},
 
 	openSearch: function() {
 		this.searchToggleBtn.addEventListener('click', () => {
 			this.search.classList.toggle('header-search--open');
 		});
+	},
+
+	openNav: function() {
+		const _this = this;
+		$('.js-menu').click(function() {
+			const $this = $(this);
+			$this.toggleClass('open');
+			$('.nav').toggleClass('open');
+			if ($('.nav').hasClass('open')) {
+				_this.disableScroll();
+			} else {
+				_this.enableScroll();
+			}
+		  });
 	}
 };
 
