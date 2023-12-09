899e18d73e126a22a8dcf08e1c4d2ca6d9a8eac3
DESKTOP-S9HUM6I\nikol <nikolic.luka.ns@gmail.com>
2023-12-09 17:39:37 +0100

    fix price calculation

--- PerAspera/wwwroot/js/shop.js
+++ PerAspera/wwwroot/js/shop.js
@@ -62,7 +62,7 @@ $(document).ready(function () {
         priceLabel.text("");
         var price;
         $(".shop-card__content").find('h3').filter(function () {
-            if ($.trim($(this).text()).replace(/\s+/g, '') === updatedItemName) {
+            if ($.trim($(this).text()).replace(/\s+/g, '').replace(/[^a-zA-Z0-9]/g, '') === updatedItemName) {
                 price = $(this).closest('.shop-card__content').find('.shop-card__price').text().replace(/\D/g, '')
             }
         });
